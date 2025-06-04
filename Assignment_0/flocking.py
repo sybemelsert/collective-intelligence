dd8db7173e93b063165a4d292735ad9387174e4a
Rushil Nalekar <r.m.nalekar@student.vu.nl>
2025-06-04 15:41:03 +0200

    New flocking start

--- Assignment_0/flocking.py
+++ Assignment_0/flocking.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 from vi import Agent, Config, Simulation
-from pygame.math import Vector2
+
 
 @dataclass
 class FlockingConfig(Config):
@@ -11,52 +11,13 @@ class FlockingConfig(Config):
     separation_weight: float = 1
 
 
-class FlockingAgent(Agent):
+class FlockingAgent(Agent[FlockingConfig]):
+    # By overriding `change_position`, the default behaviour is overwritten.
+    # Without making changes, the agents won't move.
     def change_position(self):
-        neighbors = self.in_proximity_accuracy()
-        neighbors = [agent for agent, _ in neighbors]
+        self.there_is_no_escape()
 
-        if not neighbors:
-            return
-
-        v_boid = self.move
-        x_boid = self.pos
-
-        # Alignment
-        v_avg = sum((agent.move for agent in neighbors), Vector2()) / len(neighbors)
-        alignment = v_avg - v_boid
-
-        # Separation
-        separation = sum((x_boid - agent.pos for agent in neighbors), Vector2()) / len(neighbors)
-
-        # Cohesion
-        x_avg = sum((agent.pos for agent in neighbors), Vector2()) / len(neighbors)
-        f_c = x_avg - x_boid
-        cohesion = f_c - v_boid
-
-        # Weights & constants
-        α = self.config.alignment_weight
-        β = self.config.separation_weight
-        γ = self.config.cohesion_weight
-        mass = getattr(self.config, "mass", 20)
-        delta_time = getattr(self.config, "delta_time", 3)
-        max_speed = getattr(self.config, "max_velocity", 2.0)
-
-        # Total force
-        f_total = (α * alignment + β * separation + γ * cohesion) / mass
-
-        # Velocity update
-        self.move += f_total
-
-        # Apply min(Move.length, MaxVelocity) * Move.normalize()
-        speed = min(self.move.length(), max_speed)
-        self.move = self.move.normalize() * speed if self.move.length() != 0 else Vector2()
-
-        # Position update
-        self.pos += self.move * delta_time
-
-
-        
+        # TODO: Modify self.move and self.pos accordingly.
 
 
 (
@@ -64,6 +25,6 @@ class FlockingAgent(Agent):
         # TODO: Modify `movement_speed` and `radius` and observe the change in behaviour.
         FlockingConfig(image_rotation=True, movement_speed=1, radius=50)
     )
-    .batch_spawn_agents(100, FlockingAgent, images=["Assignment_0/images/triangle.png"])
+    .batch_spawn_agents(100, FlockingAgent, images=["images/triangle.png"])
     .run()
 )
